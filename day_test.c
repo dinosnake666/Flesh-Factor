23104e39303725151d673fb3dc3b8f133524380e
dinosnake666 <158581931+dinosnake666@users.noreply.github.com>
2024-07-23 16:46:11 +0100

    wip : hp, day and eating.

---
+++ day_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+    int bread;
+    //unsigned int hp;
+
+    day = 7;
+    bread = 10;
+    while (day > 0)
+    {
+        printf("Day : %d\n", day);
+        day--;
+        printf("Do you want to eat bread ? : ");
+        scanf()
+    }
+    if (day == 0)
+    {
+        printf("Day : %d        This week has ended.", day);
+    }
+}
