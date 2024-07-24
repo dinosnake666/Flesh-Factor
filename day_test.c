7744dda520c90908413b859321375f8e99114700
dinosnake666 <158581931+dinosnake666@users.noreply.github.com>
2024-07-24 13:13:31 +0100

    eating and hp mechanics.
    Implemented eating and hp mechanic. Next is checking for hp == 0 and implement user input (without goto if possible).

--- day_test.c
+++ day_test.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-int main()
+int    main(void)
 {
-    int day;
-    int bread;
-    //unsigned int hp;
+    int    day;
+    int    bread;
+    char    eat;
+    int    flag;
+    unsigned int    hp;
 
-    day = 7;
+    day = 10;
     bread = 10;
+    hp = 100;
     while (day > 0)
     {
         printf("Day : %d\n", day);
         day--;
-        printf("Do you want to eat bread ? : ");
-        scanf()
+        printf("Do you want to eat bread ? [y/n] : ");
+        scanf("%c", &eat);
+        if (eat == 'y')
+        {
+            bread--;
+            printf("You consumed bread.\tRemaining bread : %d\n", bread);
+            if (hp < 100)
+                hp = hp + 10;
+            printf("Current hp : %u\n", hp);
+        }
+        else if (eat == 'n')
+        {
+            hp = hp - 10;
+            printf("You didn't eat bread... maybe for later.\n");
+            printf("Remaining bread : %d\n", bread);
+            printf("Current hp : %u\n", hp);
+        }
+        else 
+        {
+            printf("You little prankster, I'm going to sell all your data since you're SO smart...\n");
+            return(0);
+        }
     }
     if (day == 0)
     {
-        printf("Day : %d        This week has ended.", day);
+        printf("Day : %d\tThis week has ended.\n", day);
     }
 }
