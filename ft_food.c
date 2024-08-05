465f8682c6a34d77587edadadc4e500c8829dade
dinosnake666 <ahamada0506@gmail.com>
2024-08-05 16:32:36 +0100

    randomness added

--- ft_food.c
+++ ft_food.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-struct food {
+struct available_food {
   char *name;
   char *state;
-  int hp_change;
+  char *hp_change;
 };
 
-int main() {
-  struct food stew = {"stew", "umami", 20};
-  struct food mushroom = {"mushroom", "poisonous", -5};
-  struct food meat = {"meat", "rotten", -15};
+void ft_food(void)
+{
+    struct available_food encountered_food[] =
+    {
+        {"stew", "umami", "+20"},
+        {"mushroom", "poisonous", "-5"},
+        {"meat", "rotten", "-15"},
+        {"bread", "stale", "+2"},
+        {"GARLIC", "LEGENDARY", "+5000"},
+    };
+    
+    srand(time(NULL) + 91028050);
+    int chosen = rand() % 5;
 
-  printf("You found [%s %s].\n", mushroom.state, mushroom.name);
-  printf("You found [%s %s].\n", meat.state, meat.name);
-  printf("You found [%s %s].\n", stew.state, stew.name);
-
-  return (0);
+    printf("You found [%s %s]. %s HP\n", encountered_food[chosen].state, encountered_food[chosen].name, encountered_food[chosen].hp_change);
 }
