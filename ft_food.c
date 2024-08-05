e3b8f3140ddadd6ea82af7cf8d4d8e8add9ad956
dinosnake666 <ahamada0506@gmail.com>
2024-08-05 17:10:38 +0100

    improved ft_food.c

--- ft_food.c
+++ ft_food.c
@@ -3,24 +3,26 @@
 #include <stdlib.h>
 
 struct available_food {
-  char *name;
-  char *state;
-  char *hp_change;
+  char  *name;
+  char  *state;
+  int   hp_change;
 };
 
-void ft_food(void)
+int ft_food(void)
 {
-    struct available_food encountered_food[] =
+    struct available_food encountered_food[] = //this array of struct var allows to choose randomly;
     {
-        {"stew", "umami", "+20"},
-        {"mushroom", "poisonous", "-5"},
-        {"meat", "rotten", "-15"},
-        {"bread", "stale", "+2"},
-        {"GARLIC", "LEGENDARY", "+5000"},
+        {"stew", "hot", 20},
+        {"mushroom", "poisonous", -5},
+        {"meat", "rotten", -15},
+        {"bread", "stale", 2},
+        {"GARLIC", "LEGENDARY", 5000},
     };
     
-    srand(time(NULL) + 91028050);
+
+    srand(time(NULL) + 91028050);//silly number :)
     int chosen = rand() % 5;
 
-    printf("You found [%s %s]. %s HP\n", encountered_food[chosen].state, encountered_food[chosen].name, encountered_food[chosen].hp_change);
+    printf("You found [%s %s].\n", encountered_food[chosen].state, encountered_food[chosen].name);
+    return(encountered_food[chosen].hp_change);
 }
