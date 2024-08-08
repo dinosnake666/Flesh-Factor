e9df173c5a0fc4b40f964ba8155babd7816d2029
dinosnake666 <ahamada0506@gmail.com>
2024-08-08 12:55:38 +0100

    ft_eat_food.c simplified ; reworking wip

--- ft_eat_food.c
+++ ft_eat_food.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 
-int ft_letter_check(char *str);// declaring input check function;
+int ft_letter_check(char *str);//declaring input check function;
 
-void ft_eat_food(int hp)
+int ft_eat_food(void)
 {
     struct available_food//declaring food struct;
     {
@@ -22,29 +22,27 @@ void ft_eat_food(int hp)
         {"pizza slice", "mouldy and half-eaten", -60},
         {"noodle cup", "glowing green", -99},
         {"GARLIC BREAD", "LEGENDARY", 500},
-        {"watermelon slices", "fresh", 45}
+        {"watermelon slices", "fresh", 45},
+        {"tuna bits", "canned", 9},
+        {"mouldy hamburger", "canned", -16}
     };
     
     srand(time(NULL));
-    const int chosen = rand() % 9;//food encounter is randomised;
+    const int chosen = rand() % 11;//food encounter is randomised;
     printf("You found [%s %s].\n", encountered_food[chosen].state, encountered_food[chosen].name);
-    
     
     char food_choice[2];
     printf("Do you want to eat the food ? ");//player choice;
     if (ft_letter_check(food_choice))
     {
-        hp = hp + encountered_food[chosen].hp_mod;//remember : adding a neg int == substraction;
         printf("You ate [%s %s]...\t", encountered_food[chosen].state, encountered_food[chosen].name);
         
         if(encountered_food[chosen].hp_mod < 0)
             printf("You lost %d HP !\n", encountered_food[chosen].hp_mod);
         else
             printf("You gained %d HP!\n", encountered_food[chosen].hp_mod);
-        
     }
     else
         printf("You decided to not eat... Maybe later ?\n");
-    printf("HP : %d\n", hp);
-    //return(hp);
+    return(encountered_food[chosen].hp_mod);
 }
